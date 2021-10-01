09517959ddfb5a643907cbf9d74255a8d76ed9fe
Lázaro Queiroz <lazaroq11@hotmail.com>
2021-10-01 14:47:16 -0300

    fix: click modal and double click like

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
 let openSideMenu = document.querySelector(".side-menu__button--hamburguer");
+let modal = document.querySelector(".modal");
+let closeModal = modal.querySelector(".close");
+let modalImg = modal.querySelector(".modal__image");
+let selectedImage = null;
 
-openSideMenu.addEventListener("click",()=>{
+    openSideMenu.addEventListener("click",()=>{
     let sideMenu = document.querySelector(".side-menu");
     let btnimg = openSideMenu.querySelector(".side-menu__icon");
     
@@ -14,33 +18,52 @@ openSideMenu.addEventListener("click",()=>{
 });
 
     document.querySelectorAll(".item__image").forEach(img => {
-    img.addEventListener("dblclick",()=>{
-        let like = img.previousElementSibling;
-        if(like.classList.contains("item__like")){
-            if(like.classList.contains("hidden")){
-                like.classList.remove("hidden")
-            }else{
-                like.classList.add("hidden")
-            }
-        }
-
-    });
-    
+    let clickCounter=0
     img.addEventListener("click",()=>{
-        let modal = document.querySelector(".modal");
-
-        if(modal.classList.contains("hidden")){
-            modal.classList.remove("hidden")
-            modal.querySelector(".modal__image").src=img.src;
-        }else{
-            modal.classList.add("hidden")
-            modal.querySelector(".modal__image").src="";
+        clickCounter++;
+        if(clickCounter === 1){
+            clickerTime = setTimeout(()=>{
+                clickCounter = 0;
+                openModal(img);
+            },400);
+        }else if(clickCounter===2){
+            clearTimeout(clickerTime)
+            clickCounter = 0
+            likeHandler(img);
         }
-        
-     
-    }
-    )
+   
+    });
 });
 
+    function likeHandler(img){
+    let like = img.previousElementSibling;
+    if(like.classList.contains("item__like")){
+        if(like.classList.contains("hidden")){
+            like.classList.remove("hidden")
+        }else{
+            like.classList.add("hidden")
+        }
+    }
+}
 
-   
+    function openModal(img){
+            modal.classList.remove("hidden")
+            modal.querySelector(".modal__image").src=img.src;   
+        }
+
+
+
+    
+
+    closeModal.addEventListener("click",()=>{
+        modal.classList.add("hidden")
+        modal.querySelector(".modal__image").src="";
+    })
+
+
+
+    modalImg.addEventListener("dblclick",()=>{
+        likeHandler()
+    })
+
+ 
